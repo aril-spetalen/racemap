8c32deb259e77d70c8794327b13b9de4a4ef1d73
Aril Spetalen <aril.spetalen@gmail.com>
2020-02-16 15:18:43 +0100

    decided on yr.js coordinates method.

--- server/src/yr.js
+++ server/src/yr.js
@@ -1,36 +1,14 @@
 import fetch from 'node-fetch';
 
-/*
-const yr = {
-  coordinates: async (location) => {
-    let result = {};
-    await fetch(`http://stadnamn.nrk.no/api/yr/search/${location}`)
-      .then(res => res.json())
-      .then((json) => {
-      if (json && json._embedded && json._embedded.location && json._embedded.location.length > 0) {
-        result = {
-          location: location,
-          coordinate: json._embedded.location[0].coordinate
-
-        }
-      }
-    });
-
-    return result;
-  }
-};
-*/
-
 const coordinates = async (location) => {
-    let result = {};
-    await fetch(`http://stadnamn.nrk.no/api/yr/search/${location}`)
-      .then(res => res.json())
-      .then((json) => {
+  let result = {};
+  await fetch(`http://stadnamn.nrk.no/api/yr/search/${location}`)
+    .then(res => res.json())
+    .then((json) => {
       if (json && json._embedded && json._embedded.location && json._embedded.location.length > 0) {
         result = {
           location: location,
           coordinate: json._embedded.location[0].coordinate
-
         }
       }
     });
@@ -42,3 +20,10 @@ const coordinates = async (location) => {
 module.exports = {
   coordinates
 }
+
+/*
+ * // API:
+coordinates('oslo')
+    .then(console.log);
+    // will output location with coordinates.
+*/
--- server/test/details.test.js
+++ server/test/details.test.js
@@ -16,16 +16,17 @@ let knsDetails = cd.getDetails(knsFile);
 let clubs = [asfDetails, knsDetails]
 
 
-test('coordinates of aalesund sf is { lat: 62.46937, lon: 6.20505 }', () => {
-  let coord = {};
-  Promise.resolve((coord) => {
-    coordinates('Nørvevika')
+
+test('coordinates of Aalesund SF is { lat: 62.46937, lon: 6.20505 }', () => {
+  coordinates('Nørvevika')
     .then((result) => {
-      expect(result).toBe({ lat: 62.46937, lon: 6.20505 });
+      expect(result.coordinate).toEqual({ lat: 62.46937, lon: 6.20505 });
     })
+    .catch((err) => {
+      console.log('an exception occurred:', err);
   });
-  // expect(asfDetails.coordinates).toBe({ lat: 62.46937, lon: 6.20505 });
 });
+
 
 test('address of aalesund sf is Nørvevika', () => {
   expect(asfDetails.address).toBe('Nørvevika');
